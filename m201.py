ab737007f61214d86d306b79e827d8e8cf31a195
Alec Thomson (S&A, Kensington WA) <alec.thomson@csiro.au>
2024-02-20 17:41:59 +0900

    Adapt single RM models

--- m201.py
+++ m201.py
@@ -1,7 +1,10 @@
 #=============================================================================#
 #                          MODEL DEFINITION FILE                              #
 #=============================================================================#
+import bilby
 import numpy as np
+from bilby.core.prior import Constraint, PriorDict
+from functools import partial
 
 
 #-----------------------------------------------------------------------------#
@@ -26,46 +29,67 @@ def model(pDict, lamSqArr_m2):
     return quArr
 
 
-#-----------------------------------------------------------------------------#
-# Parameters for the above model.                                             #
+# -----------------------------------------------------------------------------#
+# Priors for the above model.                                                 #
+# See https://lscsoft.docs.ligo.org/bilby/prior.html for details.             #
 #                                                                             #
-# Each parameter is defined by a dictionary with the following keywords:      #
-#   parname    ...   parameter name used in the model function above          #
-#   label      ...   latex style label used by plotting functions             #
-#   value      ...   value of the parameter if priortype = "fixed"            #
-#   bounds     ...   [low, high] limits of the prior                          #
-#   priortype  ...   "uniform", "normal", "log" or "fixed"                    #
-#   wrap       ...   set > 0 for periodic parameters (e.g., for an angle)     #
-#-----------------------------------------------------------------------------#
+# -----------------------------------------------------------------------------#
+def converter(parameters, nterms):
+    """
+    Function to convert between sampled parameters and constraint parameter.
+
+    Parameters
+    ----------
+    parameters: dict
+        Dictionary containing sampled parameter values, 'RM1_radm2', 'RM1_radm2',
+        'fracPol1', 'fracPol2'
+
+    Returns
+    -------
+    dict: Dictionary with constraint parameter 'delta_RM1_RM2_radm2' and 'sum_p1_p2' added.
+    """
+    converted_parameters = parameters.copy()
+    for i in range(nterms-1):
+        converted_parameters[f"delta_RM{i+1}_{i+2}_radm2"] = (
+            parameters[f"RM{i+1}_radm2"] - parameters[f"RM{i+2}_radm2"]
+        )
+        converted_parameters[f"sum_p{i+1}_{i+2}"] = parameters[f"fracPol{i+1}"] + parameters[f"fracPol{i+2}"]
+    return converted_parameters
+
 terms = 2
-inParms = []
+priors = PriorDict(conversion_function=partial(converter, nterms=terms))
 for i in range(terms):
-    inParms += [
-    {"parname":   f"fracPol_{i}",
-     "label":     f"$p_{{0,{i}}}$",
-     "value":     0.1+np.random.rand()*10,
-     "bounds":    [0.001, 1.0],
-     "priortype": "uniform",
-     "wrap":      0},
+    priors[f"fracPol{i+1}"] = bilby.prior.Uniform(
+        minimum=0.001,
+        maximum=1.0,
+        name=f"fracPol{i+1}",
+        latex_label=r"$p_1$",
+    )
+    priors[f"psi0{i+1}_deg"] = bilby.prior.Uniform(
+        minimum=0,
+        maximum=180.0,
+        name=f"psi0{i+1}_deg",
+        latex_label=fr"$\psi_{{0,{i+1}}}$ (deg)",
+        boundary="periodic",
+    )
 
-    {"parname":   f"psi0_{i}_deg",
-     "label":     f"$\psi_{{0,{i}}}$ ($^\circ$)",
-     "value":     0.0+np.random.rand()*10,
-     "bounds":    [0.0, 180.0],
-     "priortype": "uniform",
-     "wrap":      1},
-
-    {"parname":   f"RM_{i}_radm2",
-     "label":     f"$\phi_{{0,{i}}}$ (rad m$^{{-2}}$)",
-     "value":     0.0+np.random.rand()*10,
-     "bounds":    [-1000.0, 1000.0],
-     "priortype": "uniform",
-     "wrap":      0},
-    ]
-
-
-#-----------------------------------------------------------------------------#
-# Arguments controlling the Nested Sampling algorithm                         #
-#-----------------------------------------------------------------------------#
-nestArgsDict = {"n_live_points": 1000,
-                "verbose": False}
+    priors[f"RM{i+1}_radm2"] = bilby.prior.Uniform(
+        minimum=-1100.0,
+        maximum=1100.0,
+        name=f"RM{i+1}_radm2",
+        latex_label=fr"$\phi_{i+1}$ (rad m$^{{-2}}$)",
+    )
+if terms > 1:
+    for i in range(terms-1):
+        priors[f"delta_RM{i+1}_{i+2}_radm2"] = Constraint(
+            minimum=0,
+            maximum=1100.0/terms,
+            name=f"delta_RM{i+1}_{i+2}_radm2",
+            latex_label=fr"$\Delta\phi_{i+1,i+2}$ (rad m$^{{-2}}$)",
+        )
+    priors["sum_p"+"_".join([f"{i+1}" for i in range(terms)])] = Constraint(
+        minimum=0.001,
+        maximum=1.0,
+        name="sum_p"+"_".join([f"{i+1}" for i in range(terms)]),
+        latex_label=r"$p" + r"p_".join([fr"{i+1}+" for i in range(terms)]) + r"$",
+    )
